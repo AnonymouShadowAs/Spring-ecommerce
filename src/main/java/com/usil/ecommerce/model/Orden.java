63ff33a72f0c836f1068547f8919a0655d07045e
joseph <guille_anonymous25@hotmail.com>
2022-11-09 17:40:14 -0500

    crear clases model, mapeo clases a tablas con jpa, integrado la diseño y vistas al proyecto

--- src/main/java/com/usil/ecommerce/model/Orden.java
+++ src/main/java/com/usil/ecommerce/model/Orden.java
@@ -1,23 +1,45 @@
 package com.usil.ecommerce.model;
 
 import java.util.Date;
+import java.util.List;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "ordenes")
 public class Orden {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	private String numero;
 	private Date fechaCreacion;
 	private Date fechaRecibida;
+
+	private double total;
+	
+	@ManyToOne
+	private Usuario usuario;
+	
+	@OneToMany(mappedBy = "orden")
+	private List<DetalleOrden> detalle;
 	
 	public Orden() {
-		// TODO Auto-generated constructor stub
+	
 	}
 
-	public Orden(Integer id, String numero, Date fechaCreacion, Date fechaRecibida) {
+	public Orden(Integer id, String numero, Date fechaCreacion, Date fechaRecibida, double total) {
 		super();
 		this.id = id;
 		this.numero = numero;
 		this.fechaCreacion = fechaCreacion;
 		this.fechaRecibida = fechaRecibida;
+		this.total = total;
 	}
 
 	public Integer getId() {
@@ -52,13 +74,37 @@ public class Orden {
 		this.fechaRecibida = fechaRecibida;
 	}
 
+	public double getTotal() {
+		return total;
+	}
+
+	public void setTotal(double total) {
+		this.total = total;
+	}
+	
+
+	public Usuario getUsuario() {
+		return usuario;
+	}
+
+	public void setUsuario(Usuario usuario) {
+		this.usuario = usuario;
+	}
+	
+
+	public List<DetalleOrden> getDetalle() {
+		return detalle;
+	}
+
+	public void setDetalle(List<DetalleOrden> detalle) {
+		this.detalle = detalle;
+	}
+
 	@Override
 	public String toString() {
 		return "Orden [id=" + id + ", numero=" + numero + ", fechaCreacion=" + fechaCreacion + ", fechaRecibida="
-				+ fechaRecibida + "]";
+				+ fechaRecibida + ", total=" + total + "]";
 	}
-	
-	
 	
 
 }
--- src/main/java/com/usil/ecommerce/model/Producto.java
+++ src/main/java/com/usil/ecommerce/model/Producto.java
@@ -1,6 +1,18 @@
 package com.usil.ecommerce.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "productos")
+
 public class Producto {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	private String nombre;
 	private String descripcion;
@@ -8,10 +20,15 @@ public class Producto {
 	private double precio;
 	private int cantidad;
 	
+	@ManyToOne
+	private Usuario usuario;
+	
 	public Producto() {
 	}
 
-	public Producto(Integer id, String nombre, String descripcion, String imagen, double precio, int cantidad) {
+
+	public Producto(Integer id, String nombre, String descripcion, String imagen, double precio, int cantidad,
+			Usuario usuario) {
 		super();
 		this.id = id;
 		this.nombre = nombre;
@@ -19,6 +36,7 @@ public class Producto {
 		this.imagen = imagen;
 		this.precio = precio;
 		this.cantidad = cantidad;
+		this.usuario = usuario;
 	}
 
 	public Integer getId() {
@@ -67,6 +85,16 @@ public class Producto {
 
 	public void setCantidad(int cantidad) {
 		this.cantidad = cantidad;
+	}
+
+	
+	
+	public Usuario getUsuario() {
+		return usuario;
+	}
+
+	public void setUsuario(Usuario usuario) {
+		this.usuario = usuario;
 	}
 
 	@Override
